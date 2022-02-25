f762dbf6380b0b17832627e4ad186ab778df3d4e
Trever <treverjtaylor1@gmail.com>
2022-02-25 11:28:08 -0600

    CAC-102 Exercise 2

---
+++ exercises/exercise2.py
@@ -0,0 +1,34 @@
+
+# 1. SIMPLE FUNCTION
+def simple():
+    """Display 5 variables with different messages"""
+    message1 = 'Hello!'
+    message2 = 'how are you?'
+    message3 = 'I am doing well.'
+    message4 = 'I hope your day is going well.'
+    message5 = 'See you Later!'
+    print(message1, message2, message3, message4, message5)
+
+simple()
+
+# 2.
+
+def simple2():
+    message = 'hello'
+    print(message)
+    message = 'how are you?'
+    print(message)
+    message = 'Hope you are doing well today.'
+    print(message)
+    message = 'Have a good day!'
+    print(message)
+
+simple2()
+
+# 3. ARGUMENTS
+def message(arg1):
+    print(arg1)
+
+message('Hello')
+
+
