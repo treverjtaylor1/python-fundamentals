7af475c782763b4215e41b5e15de892a9240883b
Trever <treverjtaylor1@gmail.com>
2022-02-25 12:51:22 -0600

    CAC-102 Exercise 2

---
+++ exercises/exercise3.py
@@ -0,0 +1,26 @@
+# 2-3 PERSONAL MESSAGE
+first_name = "Jack"
+last_name = "Beanstock"
+full_name = f"{first_name} {last_name}"
+print(f"Hello {full_name}, how are you doing today?")
+
+# 2-4 NAME CASES
+name = "Jack"
+print(f"{name.upper()}")
+print(f"{name.lower()}")
+print(f"{name.title()}")
+
+# 2-5 FAMOUS QUOTES
+print('Batman said "This city is ready to believe in good"')
+
+# 2-6 FAMOUS QUOTES 2
+famous_person = 'Batman'
+message = "This city is ready to believe in good"
+print(f'{famous_person} said "{message}"')
+
+# 2-7 STRING NAMES
+name = '\tJim\n'
+print(name)
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
