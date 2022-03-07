e67226e6e70ad979e797249d52b1863b95a6b4be
Trever <treverjtaylor1@gmail.com>
2022-03-07 17:07:19 -0600

    CAC-102 LESSON 2

---
+++ exercises/exercise7.py
@@ -0,0 +1,34 @@
+#4-3
+
+def twenty_count():
+    numbers = list(range(1,21))
+
+    for number in numbers:
+        print(number)
+twenty_count()
+
+#4-6
+def odd_numbers():
+    numbers = list(range(1, 20, 2))
+    for number in numbers:
+        print(number)
+
+odd_numbers()
+
+#4-7
+
+def multi_threes():
+    numbers = list(range(3,30,3))
+
+    for number in numbers:
+        print(number)
+
+multi_threes()
+
+#4-8
+def cube_function():
+    numbers = list(range(1,11))
+    for number in numbers:
+        print(number**3)
+
+cube_function()
