e2d223873552e4f6b969ac5feabcf5fe9bf471ed
Trever <treverjtaylor1@gmail.com>
2022-03-07 15:12:40 -0600

    CAC-102 LESSON 2

---
+++ exercises/exercise6.py
@@ -0,0 +1,65 @@
+#
+#
+#5-3
+# Part 1 & 2
+
+def alien_color(color1):
+    if color1 == 'Green':
+        print('you have just earned 5 points!')
+
+alien_color(input('What Color did you shoot down?'))
+
+#5-4
+def alien_color(color2):
+    if color2 == 'Green':
+        print('You have just earned 5 points!')
+    else:
+        print('You have just earned 10 Points!')
+
+alien_color(input('What Color did you shoot Down?'))
+
+
+#5-5
+
+def alien_color(color):
+    if color == 'Green':
+        print('You have just earned 5 points!')
+    elif color == 'Red':
+        print('You have just earned 15 points!')
+    elif color == 'Yellow':
+        print('You have just earned 10 points!')
+alien_color('')
+
+#5-6
+def stage_of_life():
+    age = 70
+    if age < 2:
+        print("this person is a baby")
+    elif age < 4:
+        print("This person is a toddler")
+    elif age < 13:
+        print("This person is a kid")
+    elif age < 20:
+        print("This person is a teenager")
+    elif age < 65:
+        print("This person is an adult")
+    else:
+        print("This person is an elder")
+
+stage_of_life()
+
+#def number_arg(arg1):
+    #arg1 = 5
+   # bool(6)
+
+#number_arg(7)
+
+
+
+
+
+
+
+
+
+
