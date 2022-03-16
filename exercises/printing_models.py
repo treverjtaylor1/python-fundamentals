229ed2e74d4e45ec4d864fc3bccdf627f9c88cef
Trever <treverjtaylor1@gmail.com>
2022-03-15 22:37:41 -0500

    CAC-102 LESON 2

---
+++ exercises/printing_models.py
@@ -0,0 +1,7 @@
+import exercise13 as pf
+
+unprinted_designs = ['phone', 'arm', 'circle']
+completed_models = []
+
+pf.print_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)
