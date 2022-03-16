87d90c42e1fdc0689e3ea7ae5efb1aef359f6933
Trever <treverjtaylor1@gmail.com>
2022-03-15 22:17:22 -0500

    CAC-102 LESON 2

---
+++ exercises/exercise12.py
@@ -0,0 +1,67 @@
+# 10-6
+
+try:
+    x = input("Give me a number: ")
+    x = int(x)
+
+    y = input("Give me another number: ")
+    y = int(y)
+except ValueError:
+    print("Sorry, I really needed a number.")
+else:
+    sum = x + y
+    print(f"The sum of {x} and {y} is {sum}.")
+
+# 10-7
+
+print("Enter 'Q' at any time to quit.")
+
+while True:
+    try:
+        x = input("Give me a number: ")
+        if x == 'q':
+            break
+
+        x = int(x)
+
+        y = input("Give me another number: ")
+        if y == 'q':
+            break
+
+        y = int(y)
+
+    except ValueError:
+        print("Sorry, I really needed a number.")
+
+    else:
+        sum = x + y
+        print(f"The sum of {x} and {y} is {sum}.")
+
+#10-8
+
+filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+    print(f"\nReading file: {filename}")
+    try:
+        with open(filename) as f:
+            contents = f.read()
+            print(contents)
+    except FileNotFoundError:
+        print("  Sorry, I can't find that file.")
+
+#10-9
+filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+
+    try:
+        with open(filename) as f:
+            contents = f.read()
+
+    except FileNotFoundError:
+        pass
+
+    else:
+        print(f"\nReading file: {filename}")
+        print(contents)
